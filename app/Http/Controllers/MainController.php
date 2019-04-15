74244a42952ea7362490bedbb729de246a5adab5
Victor Hamil Diaz <hamil151@hotmail.com>
2019-04-15 10:40:23 -0400

    + render

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -11,6 +11,6 @@ class MainController extends Controller
     }
 
     public function login(Request $request){
-        return view('modals.login');
+        return view('modals.login')->render();
     }
 }
