a7d563b46331b79e4970c38c1c650e4f9b7efece
Victor Hamil Diaz <hamil151@hotmail.com>
2019-05-05 20:48:04 -0400

    component article

--- resources/js/Script.js
+++ resources/js/Script.js
@@ -6,7 +6,7 @@
 
  import React from 'react';
  import ReactDOM from 'react-dom';
-import '../css/Style.css';
+import Article from './components/Article'
 
 $(document).ready(function(){
     console.log("El documento 'Script' de Historia Dominicana está listo.");
@@ -83,35 +83,5 @@ $(document).ready(function(){
         });
     });
 
-    const Article = (
-        <div>
-            <h2 className="center bold ">Historia de la República Dominicana</h2>
-                        <div className="magic"></div>
-                    <div className="desarrolloArticulo " style="overflow: hidden !important">
-                        <p className="center letras">La República Dominicana se fundó en el año 1844 después de muchos años de opresión bajo los Haitianos.
-                        Durante los 22 años que precedieron a la independencia, toda la isla de La Española estuvo bajo el dominio de Haití.
-                        Después de los esfuerzos hechos por patriotas dominicanos para independizar el país del dominio haitiano, varias acciones
-                        militares ocurridas entre 1844 y 1856 terminaron por consolidar a la república como un nuevo estado. Los haitianos intentaron
-                        varias veces volver a dominar la recién creada república con resultados fallidos hasta que en 1867 Haití reconoció la
-                        independencia dominicana.</p>
-                        <div>
-                            <img src="/imagenes/padresDeLaPatria.jpeg" alt="Padres de la patria" className="padresdelapatria"/>
-                        </div>
-                        <p>
-                            La palabra Dominicana proviene del latín Dominicus, que significa Domingo. Sin embargo, la isla tiene este nombre por
-                            Santo Domingo de Guzmán, fundador de la Orden de los Dominicos.
-                        </p>
-                        <p>
-                            Esta es una introducción a todo lo que usted será capaz de ver en esta página web, para seguir viendo más puede darle
-                            al menú de la parte superior izquierda y elegir un evento que usted desee (estos eventos están ordenados de forma cronologica).
-                        </p>
-                    </div>
-        </div>
-    );
-
-    let helloWorld  = (
-        <Article className="marginCenter maxWidth letras first" />
-    );
-
-    ReactDOM.render(helloWorld, document.getElementById('letras'));
+    ReactDOM.render(Article, document.getElementById('letras'));
 });
