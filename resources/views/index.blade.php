4e8f79a6031a9e4f2d37cc9c905097b2da450098
Victor Hamil Diaz <hamil151@hotmail.com>
2019-05-17 16:48:27 -0400

    viendo el session

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@ and open the template in the editor......
 <html>
     <head>
         <meta charset="UTF-8">
-    <title>{{ session('Historia.Dominicana') }}</title>
+    <title>{{ session('Historia') }}</title>
         <!--Css-->
         <link rel="stylesheet" type="text/css" href="css/Style.css">
         <link rel="stylesheet" type="text/css" href="css/modalLogin.css">
